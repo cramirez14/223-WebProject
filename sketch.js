2958c0b5cbbd42982e1466354d88a59df9fa3e5b
cramir86@stu.jjc.edu <cramir86@stu.jjc.edu>
2024-11-24 13:37:18 -0600

    Fourth Commit

--- sketch.js
+++ sketch.js
@@ -1,32 +1,40 @@
 let player;
 let bottomFloor;
+let platforms;
 
 function setup() {
 	new Canvas(400, 600);
 	world.gravity.y = 10;
 
 	bottomFloor = new Sprite(200, 600, 400, 10, 'static');
-	bottomFloor.color = 'green';
+	bottomFloor.color = 'gray';
 
 	player = new Sprite(200, 570, 50, 50, 'dynamic');
 	player.rotationLock = true;
 
 	platforms = new Group();
 
-	for(let i = 0; i < 6; i++) {
+	for(let i = 1; i < 4; i++) {
 		let platform = new platforms.Sprite();
 		platform.w = 100;
 		platform.h = 10;
 		platform.x = random(0, 400);
-		platform.y = i * 100;
+		platform.y = player.y - (i * 100);
 		platform.collider = 'static';
 		platform.color = 'gray';
 	}
+
 }
 
 function draw() {
 	clear();
 	background(0);
+
+	camera.on();
+	camera.x = player.x;
+	camera.y = player.y;
+
+	player.image = '🧑‍🚀';
 
 	if (kb.pressing('left')) {
 		player.x -= 5;
@@ -37,4 +45,15 @@ function draw() {
 	if (kb.presses('up') && player.colliding(bottomFloor) || kb.presses('up') && player.colliding(platforms)) {
 		player.vel.y -= 7;
 	}
+
+	if (player.y < platforms[platforms.length - 1].y && player.colliding(platforms)) {
+		let platform = new platforms.Sprite();
+		platform.w = 100;
+		platform.h = 10;
+		platform.x = random(0, 400);
+		platform.y = player.y - 100;
+		platform.collider = 'static';
+		platform.color = 'gray';
+	}
+
 }
